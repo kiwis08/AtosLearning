d98eee66c57e608e339501edaf0783948883bca6
ArturoDuCas <a00833516@tec.mx>
2023-06-14 00:31:13 -0600

    Add CurrentUser to CreateExam page

--- AtosLearning/Pages/CrearExamen.cshtml.cs
+++ AtosLearning/Pages/CrearExamen.cshtml.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using AtosLearning.Models;
+using System.Text.Json;
+
 
 namespace AtosLearning.Pages;
-
 public class CrearExamen : PageModel
 {
+    public User CurrentUser;
     public void OnGet()
     {
-        
+        var userBytes = HttpContext.Session.Get("User");
+        var user = userBytes == null ? null : JsonSerializer.Deserialize<User>(userBytes);
+        CurrentUser = user;
     }
 }
