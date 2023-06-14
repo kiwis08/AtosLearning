70a2eb9cca6eddefd6f39f9dc636bd25fdafbf71
Santiago Quihui <santiagoquihui@hotmail.com>
2023-06-14 03:40:17 -0600

    feat: more than one question

--- AtosLearning/Pages/Exam.cshtml.cs
+++ AtosLearning/Pages/Exam.cshtml.cs
@@ -13,68 +13,58 @@ public class ExamModel : PageModel
     
     private readonly IConfiguration _configuration;
     
-    public IEnumerable<Subject> Subjects { get; set; } = new List<Subject>();
-    public User CurrentUser;
     [BindProperty]
-    public string ExamTitle { get; set; }
-    [BindProperty]
-    public string ExamDescription { get; set; }
+    public User CurrentUser { get; set; }
 
     [BindProperty]
     public Exam CurrentExam { get; set; }
     
     [BindProperty]
-    public int CurrentQuestionIndex { get; set; }
+    public Question CurrentQuestion { get; set; }
 
-    [BindProperty] public int QuestionsCount { get; set; } = 1;
+    [BindProperty] public int CurrentQuestionIndex { get; set; }
 
-
-    [BindProperty]
-    public int SelectedSubjectId { get; set; }
+    
 
     public ExamModel(IConfiguration configuration)
     {
         _configuration = configuration;
     }
 
-    public async Task OnGet()
+    public void OnGet()
     {
-        // Get teacher from session state
         var userBytes = HttpContext.Session.Get("User");
         var user = userBytes == null ? null : JsonSerializer.Deserialize<User>(userBytes);
         CurrentUser = user;
-
-        Subjects = await GetSubjects();
         
-        CurrentExam = new Exam();
+        
+        var examJson = TempData["NewExam"] as string;
+        var exam = examJson == null ? null : JsonSerializer.Deserialize<Exam>(examJson);
+        CurrentExam = exam ?? new Exam();
+        
+        // Save exam again in TempData
+        TempData["NewExam"] = JsonSerializer.Serialize(CurrentExam);
+        
+        CurrentQuestionIndex = TempData["CurrentQuestionIndex"] == null ? 0 : (int)TempData["CurrentQuestionIndex"];
+        
+        // Save current question index again in TempData
+        TempData["CurrentQuestionIndex"] = CurrentQuestionIndex;
+
     }
 
 
-    public async Task<List<Subject>> GetSubjects()
+
+    public async Task<bool> UploadExam(Exam exam)
     {
-        string connectionString = _configuration.GetConnectionString("atoslearning");
-        var url = connectionString + $"Subjects/teacher/{CurrentUser.Id}";
-        
-        var client = new HttpClient();
-        var response = await client.GetAsync(url);
-        var json = await response.Content.ReadAsStringAsync();
-        var subjects = JsonSerializer.Deserialize<List<Subject>>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
-        return subjects ?? new List<Subject>();
-    }
-    
-    
-    
-    public async Task<bool> UploadExam()
-    {
-        for (int i = 0; i < CurrentExam.Questions.Length; i++)
+        for (int i = 0; i < exam.Questions.Count; i++)
         {
-            CurrentExam.Questions[i].Answers[CurrentExam.Questions[i].CorrectAnswerIndex].IsCorrect = true;
+            exam.Questions[i].Answers[exam.Questions[i].CorrectAnswerIndex].IsCorrect = true;
         }
         string connectionString = _configuration.GetConnectionString("atoslearning");
         var url = connectionString + $"api/Exams/add";
         
         var client = new HttpClient();
-        var jsonExam = JsonSerializer.Serialize(CurrentExam);
+        var jsonExam = JsonSerializer.Serialize(exam);
         var content= new StringContent(jsonExam, Encoding.UTF8, "application/json");
         var response = await client.PostAsync(url, content);
         var json = await response.Content.ReadAsStringAsync();
@@ -82,12 +72,60 @@ public class ExamModel : PageModel
         return success;
     }
 
-    public async Task<IActionResult> OnPost()
+    public IActionResult OnPostSaveQuestion()
     {
+        var examJson = TempData["NewExam"] as string;
+        var exam = examJson == null ? null : JsonSerializer.Deserialize<Exam>(examJson);
+        CurrentExam = exam;
+        
+        CurrentQuestionIndex = TempData["CurrentQuestionIndex"] == null ? 0 : (int)TempData["CurrentQuestionIndex"];
+        
+        if (CurrentExam.Questions == null)
+        {
+            CurrentExam.Questions = new List<Question>();
+        }
+        
+        // Save current question to exam object
+        CurrentExam.Questions.Insert(CurrentQuestionIndex, CurrentQuestion);
+        // Reset current question
+        CurrentQuestion = new Question();
+        // Increment question index
+        CurrentQuestionIndex++;
+        // Save exam again in TempData
+        TempData["NewExam"] = JsonSerializer.Serialize(CurrentExam);
+        
+        // Save question index in TempData
+        TempData["CurrentQuestionIndex"] = CurrentQuestionIndex;
+        
+        // Return to same page
+        return RedirectToPage("Exam");
+    }
+    
+    public async Task<IActionResult> OnPostSaveExam()
+    {
+        // Get exam from TempData
+        var examJson = TempData["NewExam"] as string;
+        var exam = examJson == null ? null : JsonSerializer.Deserialize<Exam>(examJson);
+        CurrentExam = exam;
+        
+        CurrentQuestionIndex = TempData["CurrentQuestionIndex"] == null ? 0 : (int)TempData["CurrentQuestionIndex"];
+        
+        // Save current question to exam object
+        CurrentExam.Questions.Insert(CurrentQuestionIndex, CurrentQuestion);
+        // Reset current question
+        CurrentQuestion = new Question();
+        // Increment question index
+        CurrentQuestionIndex++;
 
-        CurrentExam.SubjectId = SelectedSubjectId;
-        await UploadExam();
-        return RedirectToPage("Homepage");
+        // Upload exam to database
+        var success = await UploadExam(CurrentExam);
+        // If upload was successful, redirect to homepage
+        if (success)
+        {
+            return RedirectToPage("Homepage");
+        }
+        // Otherwise, return to same page
+        return RedirectToPage("Exam");
     }
 
 }
